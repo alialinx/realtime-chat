85eaa62d9b65e0a3dde1a1e5a3397f3fa2a79471
alialinxz <alialinxz@gmail.com>
2026-01-13 22:58:01 +0300

    feat(ws): add  connection_manager.py,ws.py and detail

--- app/api/ws/connection_manager.py
+++ app/api/ws/connection_manager.py
@@ -1,8 +1,52 @@
-from typing import Dict, List
+from typing import Dict, List, Set
 from fastapi import WebSocket
 
 class ConnectionManager:
     def __init__(self):
 
-        self.active_connections: Dict[str, List[WebSocket]] = {}
+        self.active_connections: Dict[int, Set[WebSocket]] = {}
+        self.ws_user: Dict[WebSocket, int] = {}
+        print(f"Connection manager initialized: {self.active_connections}", flush=True, )
 
+    async def connect(self, conversation_id, websocket: WebSocket, user_id):
+        await websocket.accept()
+
+        self.active_connections.setdefault(conversation_id, set()).add(websocket)
+        self.ws_user[websocket] = user_id
+        print(f"Connection accepted active_connections: {self.active_connections}", flush=True, )
+
+
+
+    async def broadcast(self, conversation_id, message):
+
+        if conversation_id not in self.active_connections:
+            return
+
+        dead: set[WebSocket] = set()
+
+        for websocket in self.active_connections[conversation_id]:
+            print(f"Broadcasting message: {message}", flush=True, )
+
+            try:
+
+                await websocket.send_json(message)
+                print("Message sent", flush=True, )
+            except Exception:
+                dead.add(websocket)
+
+        for ws in dead:
+            self.active_connections[conversation_id].discard(ws)
+
+        if len(self.active_connections[conversation_id]) == 0:
+            del self.active_connections[conversation_id]
+
+
+    def disconnect(self, conversation_id, websocket: WebSocket):
+        if conversation_id in self.active_connections:
+            print(f"Disconnecting connection: {conversation_id}", flush=True, )
+            self.active_connections[conversation_id].discard(websocket)
+
+            if len(self.active_connections[conversation_id]) == 0:
+                del self.active_connections[conversation_id]
+
+        self.ws_user.pop(websocket, None)
--- app/api/ws/ws.py
+++ app/api/ws/ws.py
@@ -1,18 +1,49 @@
 from fastapi import APIRouter
-from starlette.websockets import WebSocket
+from starlette.websockets import WebSocket, WebSocketDisconnect
 
+from app.api.functions import get_user_id_from_token, check_conversation, messages_insert_to_db
 from app.api.ws.connection_manager import ConnectionManager
+
 
 manager = ConnectionManager()
 
 router = APIRouter()
 
-@router.websocket("/ws/{conversation_id}")
+
+
+
+@router.websocket("/ws/conversations/{conversation_id}")
 async def ws_chat(websocket: WebSocket, conversation_id: int):
+
+    token = websocket.query_params.get("token")
+
+    user_id = get_user_id_from_token(token)
+
+    if not token:
+        await websocket.close(code=1008)
+        return
+
+    check_conver, conversation_message = check_conversation(conversation_id, user_id)
+
+    if not check_conver:
+        await websocket.close(code=1008)
+
+        return
+
     await manager.connect(conversation_id, websocket)
+
     try:
+
         while True:
+
             data = await websocket.receive_json()
-            await manager.broadcast(conversation_id, data)
+
+            msg = messages_insert_to_db(conversation_id, user_id, data["body"])
+
+            await manager.broadcast(conversation_id, {"type": "message.new", "data": msg})
+
+    except WebSocketDisconnect:
+        pass
+
     finally:
-        manager.disconnect(conversation_id, websocket)
+        manager.disconnect(conversation_id,websocket)
